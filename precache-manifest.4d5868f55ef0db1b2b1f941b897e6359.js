51ff155159f80a590bca8d201feb4700e0dbcce2
Rene Paul <r.paul@hccnet.nl>
2019-07-06 20:37:47 +0200

    Updates

---
+++ precache-manifest.4d5868f55ef0db1b2b1f941b897e6359.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3a5194059cc62bcb84dd4bbdfd63623",
+    "url": "/vanilla/index.html"
+  },
+  {
+    "revision": "e5a8cf7c80ed93c6bb04",
+    "url": "/vanilla/static/css/main.b18ac6f6.chunk.css"
+  },
+  {
+    "revision": "e793c4d2d0b900b13cd3",
+    "url": "/vanilla/static/js/2.512a4aac.chunk.js"
+  },
+  {
+    "revision": "e5a8cf7c80ed93c6bb04",
+    "url": "/vanilla/static/js/main.65bf90be.chunk.js"
+  },
+  {
+    "revision": "716fe70da806eb50abcf",
+    "url": "/vanilla/static/js/runtime~main.1cf28f16.js"
+  },
+  {
+    "revision": "9d0967bfe19937b123d26cde0ba6a217",
+    "url": "/vanilla/static/media/norwester.9d0967bf.ttf"
+  },
+  {
+    "revision": "90e040a0e38e49fddfae0c93e1076e2c",
+    "url": "/vanilla/static/media/town.90e040a0.jpeg"
+  }
+]);
